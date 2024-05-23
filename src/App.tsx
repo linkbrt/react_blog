0266411a7d2d473a046a877f94975cdb165dd9d1
Dan <zhulkin01@gmail.com>
2024-05-23 10:39:54 +0300

    dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, HashRouter, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticlePage from './pages/ArticlePage';
 
