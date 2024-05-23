f84776eaf616fb7d61dfeccd75b79d9d429c1268
Dan <zhulkin01@gmail.com>
2024-05-23 10:30:30 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticlePage from './pages/ArticlePage';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <HashRouter >
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/article/:id" element={<ArticlePage/>} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter >
   );
 };
 
